d07149e50826f83bbea588d4d5f7c5bd6d54af71
Naoki Nishida <domitry@gmail.com>
2014-12-26 17:12:02 +0900

    Add fill_by option to Particles

--- src/charts/particles.js
+++ src/charts/particles.js
@@ -1,15 +1,17 @@
 define([
+    "underscore",
     "components/legends",
     "utils/utils",
     "utils/datasets",
     "utils/colorbrewer"
-],function(Legends, Utils, Datasets, colorbrewer){
+],function(_, Legends, Utils, Datasets, colorbrewer){
     function Particles(data, options){
 	    this.options = {
 	        name: "Particle",
-	        color: colorbrewer.Reds[3][1],
+	        color: colorbrewer.Reds[3],
 	        size: 0.3,
-	        has_legend: true
+	        has_legend: true,
+		fill_by: null
 	    };
 
 	    if(arguments.length > 1){
@@ -22,19 +24,37 @@ define([
     }
 
     Particles.prototype.generateMesh = function(scales){
-	    var data = new Datasets.Array(this.data).raw;
+	var data = new Datasets.Array(this.data).raw;
+	var meshes = [];
+	for(var i=0;i<data.x.length;i++){
+	    var mesh = new THREE.Mesh(new THREE.SphereGeometry(this.options.size));
+	    mesh.position = new THREE.Vector3(
+		scales.x(data.x[i]),
+		scales.y(data.y[i]),
+		scales.z(data.z[i])
+	    );
+	    meshes.push(mesh);
+	}
+
+	if(this.options.fill_by){
+	    var fill_by_column = this.dataset.raw[this.options.fill_by];
+	    var range = _.map(['min', 'max'], function(name){return _[name](fill_by_column);});
+	    var domain = _.range(range[0], range[1]+1, (range[1]-range[0])/(this.options.color.length));
+	    var color_scale = d3.scale.linear()
+		    .domain(domain)
+		    .range(this.options.color);
+	    this.mesh = _.map(meshes, _.bind(function(mesh, i){
+		var color = color_scale(fill_by_column[i]);
+		mesh.material = new THREE.MeshBasicMaterial({transparent:true, color: color});
+		return mesh;
+	    }, this));
+	}else{
 	    var geometry = new THREE.Geometry();
-	    for(var i=0;i<data.x.length;i++){
-	        var mesh = new THREE.Mesh(new THREE.SphereGeometry(this.options.size));
-	        mesh.position = new THREE.Vector3(
-		        scales.x(data.x[i]),
-		        scales.y(data.y[i]),
-		        scales.z(data.z[i])
-	        );
-	        THREE.GeometryUtils.merge(geometry, mesh);
-	    }
-	    var material = new THREE.MeshBasicMaterial({transparent:true, color: this.options.color});
+	    _.each(meshes, function(mesh){THREE.GeometryUtils.merge(geometry, mesh);});
+	    var color = (_.isArray(this.options.color) ? this.options.color.shift() : this.options.color);
+	    var material = new THREE.MeshBasicMaterial({transparent:true, color: color});
 	    this.mesh = new THREE.Mesh(geometry, material);
+	}
     };
 
     Particles.prototype.getDataRanges = function(){
